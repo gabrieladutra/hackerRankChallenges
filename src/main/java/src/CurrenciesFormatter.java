49ad6968e6d20857f3ebb1a8e532306f994347c2
gabrieladutra <gabriela_ddutra@outlook.com>
2020-12-15 12:01:27 -0300

    Added getCorrencyFormatted method

---
+++ src/main/java/src/CurrenciesFormatter.java
@@ -0,0 +1,34 @@
+/* Challenge:  Java Currency Formatter
+Given a double-precision number, , denoting an amount of money, use the NumberFormat class' getCurrencyInstance method
+to convert  into the US, Indian, Chinese, and French currency formats. Then print the formatted values
+ */
+
+package src;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class CurrenciesFormatter {
+    public static String getCurrencyFormatted(NumberFormat locationFormat, double money){
+        return locationFormat.format(money);
+    }
+
+    public static void main(String[] args) {
+        double payment = 23456.77;
+
+        Locale us = Locale.getDefault();
+        Locale france = Locale.FRANCE;
+        Locale china = Locale.CHINA;
+        Locale india = new Locale("en", "in");
+
+        NumberFormat usFormatter = NumberFormat.getCurrencyInstance(us);
+        NumberFormat indiaFormatter = NumberFormat.getCurrencyInstance(india);
+        NumberFormat franceFormatter = NumberFormat.getCurrencyInstance(france);
+        NumberFormat chinaFormatter = NumberFormat.getCurrencyInstance(china);
+
+        System.out.println("US: " + getCurrencyFormatted(usFormatter,payment));
+        System.out.println("India: " + indiaFormatter.format(payment));
+        System.out.println("China: " + chinaFormatter.format(payment));
+        System.out.println("France: " + franceFormatter.format(payment));
+    }
+}
